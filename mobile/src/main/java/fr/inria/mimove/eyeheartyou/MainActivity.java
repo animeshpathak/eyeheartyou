e65c9b14b15fe17903db04e52dee20904e389a30
sarahachem <hachem.h.sara@gmail.com>
2015-04-09 11:46:21 -0700

    commutation with watch
    - Added a message receiver in mobile to receive heart rate values from
    watch
    - Fixed bug in wear app to provide heart rate value which wasn’t
    provided before

--- mobile/src/main/java/fr/inria/mimove/eyeheartyou/MainActivity.java
+++ mobile/src/main/java/fr/inria/mimove/eyeheartyou/MainActivity.java
@@ -4,14 +4,27 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.support.v4.content.LocalBroadcastManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.BroadcastReceiver;
+import android.util.Log;
+import com.google.android.gms.wearable.*;
+import android.content.*;
 
 
 public class MainActivity extends ActionBarActivity {
+    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        IntentFilter messageFilter = new IntentFilter(Intent.ACTION_SEND);
+        MessageReceiver messageReceiver = new MessageReceiver();
+        LocalBroadcastManager.getInstance(this).registerReceiver(messageReceiver, messageFilter);
     }
 
 
@@ -19,6 +32,7 @@ public class MainActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        mTextView = (TextView) findViewById(R.id.text);
         return true;
     }
 
@@ -36,4 +50,16 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public class MessageReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String message = intent.getStringExtra("heart_value");
+            Log.v("myTag", "Main activity received message: " + message);
+            // Display message in UI
+            mTextView.setText(message);
+        }
+
+    }
 }
+
--- wear/src/main/java/fr/inria/mimove/eyeheartyou/MainActivity.java
+++ wear/src/main/java/fr/inria/mimove/eyeheartyou/MainActivity.java
@@ -9,8 +9,16 @@ import android.os.Bundle;
 import android.support.wearable.view.WatchViewStub;
 import android.util.Log;
 import android.widget.TextView;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.wearable.Wearable;
+import com.google.android.gms.wearable.MessageApi;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.wearable.*;
+import com.google.android.gms.common.api.ResultCallback;
+import java.util.List;
 
-public class MainActivity extends Activity implements SensorEventListener {
+public class MainActivity extends Activity implements SensorEventListener , MessageApi.MessageListener, GoogleApiClient.ConnectionCallbacks{
 
     private TextView mTextView;
 
@@ -22,6 +30,10 @@ public class MainActivity extends Activity implements SensorEventListener {
     public MainActivity(){
 
     }
+
+
+    private GoogleApiClient client;
+
 
     @Override
     protected void onResume() {
@@ -45,6 +57,19 @@ public class MainActivity extends Activity implements SensorEventListener {
         mHeartRateSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_HEART_RATE);
 
         setContentView(R.layout.activity_main);
+
+        client = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(new GoogleApiClient.OnConnectionFailedListener() {
+                    @Override
+                    public void onConnectionFailed(ConnectionResult result) {
+                        Log.i("test", "Connection failed");
+                    }
+                })
+                .addApi(Wearable.API)
+                .build();
+
+        client.connect();
         final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
         stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
             @Override
@@ -60,11 +85,56 @@ public class MainActivity extends Activity implements SensorEventListener {
         Log.i(TAG, "--------------------------");
         //Log.i(TAG, msg);
         Log.i(TAG, ""+ sensorEvent.sensor.getType());
+    Log.i(TAG, ""+sensorEvent.values[0]);
         Log.i("live","--------------");
+        sendMessage(String.valueOf( sensorEvent.values[0]), null);
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int i) {
 
+    }
+    private void sendMessage(final String message, final byte[] payload) {
+        Log.i(TAG, "WEAR Sending message " + message);
+        Wearable.NodeApi.getConnectedNodes(client).setResultCallback(new ResultCallback<NodeApi.GetConnectedNodesResult>() {
+            @Override
+            public void onResult(NodeApi.GetConnectedNodesResult getConnectedNodesResult) {
+                List<Node> nodes = getConnectedNodesResult.getNodes();
+                for (Node node : nodes) {
+                    Log.i(TAG, "WEAR sending " + message + " to " + node);
+                    Wearable.MessageApi.sendMessage(client, node.getId(), message, payload).setResultCallback(new ResultCallback<MessageApi.SendMessageResult>() {
+                        @Override
+                        public void onResult(MessageApi.SendMessageResult sendMessageResult) {
+                            Log.i(TAG, "WEAR Result " + sendMessageResult.getStatus());
+                        }
+                    });
+                }
+
+            }
+        });
+    }
+
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Wearable.MessageApi.addListener(client, this);
+        sendMessage("/start", null);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.i(TAG, "Connection failed");
+    }
+
+    @Override
+    public void onMessageReceived(MessageEvent messageEvent) {
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Wearable.MessageApi.removeListener(client, this);
+        client.disconnect();
     }
 }
